4857dd929245b9a3ef54e2c9ccfe2811e74c6469
Rafael Barros <rafael.rph7@gmail.com>
2017-09-04 20:05:49 -0300

    Routes

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/products', (req, res) => {
-    console.log("teste nodemon");
-    res.render('products/list');
-});
+var app = require('./config/express')();
+var productsRoutes = require('./app/routes/products')(app);
 
 app.listen('3000', () => {
     console.log("Server is running...");
