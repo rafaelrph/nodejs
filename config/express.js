116557499a7ecb3f0bbb9740d9e65728e75d0bbf
Rafael Barros <rafael.rph7@gmail.com>
2017-10-18 12:05:45 -0300

    Ajuste css

--- config/express.js
+++ config/express.js
@@ -5,6 +5,7 @@ let expressValidator = require('express-validator');
 
 module.exports = function() {
     let app = express();
+    app.use(express.static('./app/public'));
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
     app.use(bodyParser.urlencoded({extended: true}));
