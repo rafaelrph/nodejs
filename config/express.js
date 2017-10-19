ea22782487f477884a8832e4e714594ff6938ef4
Rafael Barros <rafael.rph7@gmail.com>
2017-10-19 16:09:51 -0300

    Middlewares

--- config/express.js
+++ config/express.js
@@ -12,5 +12,20 @@ module.exports = function() {
     app.use(bodyParser.json());
     app.use(expressValidator());
     load('routes', {cwd: 'app'}).then('services').into(app);
+
+    //Middlewares
+    app.use(function(req, res, next){
+        res.status(404).render('erros/404');
+        next();
+    });
+
+    app.use(function(error, req, res, next) {
+        if(process.env.NODE_ENV == 'production') {
+            res.status(500).render('erros/500');
+            return;
+        }
+        next(error);
+    });
+
     return app;
 }
